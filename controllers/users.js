e60a1acd35d985faf728b6d07002703d783c1f36
Irina Isaeva <i.isaeva-Itauiti@yandex.ru>
2020-11-05 23:09:21 +0300

    for chrome and gh-p 7

--- controllers/users.js
+++ controllers/users.js
@@ -43,7 +43,7 @@ async function login(req, res, next) {
       .cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: false,
+        sameSite: true,
       });
 
     return res.send({ token, name: user.name });
